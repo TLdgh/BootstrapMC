cc22d913a68f507ece75153af8ebcd4f19fc73c7
TLdgh <teng.li2@mail.mcgill.ca>
2024-06-09 22:29:47 -0400

    Added case resampling

--- MCwithBootstrap.R
+++ MCwithBootstrap.R
@@ -1,16 +1,26 @@
 #Bootstrap can be used as a variance reduction method. See the following example of estimating the value of pi:
+library(tidyverse)
 
 t=data.frame()
-iter_boot=500
-SampleSize=seq(10, 5000, by = 10)
+iter_boot=10000
+SampleSize=seq(100, 10000, by = 250)
 
 for(ss in SampleSize){
-  x=sapply(1:iter_boot, function(x){runif(ss)})
-  y=sapply(1:iter_boot, function(x){runif(ss)})
+  x=runif(ss) 
+  y=runif(ss)
   
-  z=colMeans((x^2+y^2<=1))*4
+  #non-parametric bootstrap sample with replacement:
+  x_b=sapply(1:iter_boot, function(i){sample(x, size=ss, replace = TRUE)})
+  y_b=sapply(1:iter_boot, function(i){sample(y, size=ss, replace = TRUE)})
+  #for better result, do parametric bootstrap:
+  #x_b=sapply(1:iter_boot, function(i){runif(ss)})
+  #y_b=sapply(1:iter_boot, function(i){runif(ss)})
   
-  t=rbind(t, data.frame(l=last(z), m=mean(z)))
+  #Monte Carlo integration:
+  z=mean((x^2+y^2<=1))*4
+  z_b=colMeans((x_b^2+y_b^2<=1))*4
+  
+  t=rbind(t, data.frame(l=z, m=mean(z_b)))
 }
 
 t %>%
@@ -21,5 +31,5 @@ t %>%
   scale_color_discrete(
     name = "Method of Estimation", 
     labels = c("l" = "Without Bootstrap", "m" = "With Bootstrap"))+
-  annotate("text", x = 2500, y = 3.4, label = paste("Final Estimated Value with Bootstrap is", mean(t$m)), size = 5) +
+  annotate("text", x = median(SampleSize), y = 3.4, label = paste("Final Estimated Value with Bootstrap is", mean(t$m)), size = 5) +
   theme(plot.title = element_text(hjust = 0.5))
